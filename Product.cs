4f9f2e16ed32e0864778ee5e1e08df469dcc9ae5
Nikita <nikita2002za@mail.ru>
2022-07-21 00:26:07 +0300

    Добавьте файлы проекта.

---
+++ Product.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfizm
+{
+    class Product
+    {
+        public int Price { get; set; }
+        public string Name { get; set; }
+        public string Manufacturer { get; set; }
+
+        public virtual double GetDiscountPrice(User user)
+        {
+            if (user.Spent < 300)
+            {
+                return Price;
+            }
+
+            if (user.Spent < 1000)
+            {
+                return Price * 0.9;
+            }
+
+            return Price * 0.8;
+        }
+
+        public virtual void ToConsole()
+        {
+            Console.WriteLine(ToString()+ ":");
+            Console.WriteLine("Название: " + Name);
+            Console.WriteLine("Цена: " + Price);
+            Console.WriteLine("Производитель: " + Manufacturer);
+        }
+    }
+
+    class Steak : Product
+    {
+        public string Type { get; private set; }
+        public Steak(string type, string name, int price, string manufacturer)
+        {
+            Name = name;
+            Price = price;
+            Manufacturer = manufacturer;
+            Type = type;
+        }
+
+        public override string ToString()
+        {
+            return "Стейк";
+        }
+
+        public override void ToConsole()
+        {
+            base.ToConsole();
+            Console.WriteLine("Тип: " + Type);
+            Console.WriteLine(new String('-', 25));
+        }
+    }
+
+    class Fruit : Product
+    {
+        public int Freshness { get;  set; }
+
+        public Fruit(int freshness, string name, int price, string manufacturer)
+        {
+            Freshness = freshness;
+            Name = name;
+            Price=price;
+            Manufacturer = manufacturer;
+        }
+
+        public void LoseFreshness(int freshness)
+        {
+            Freshness--;
+        }
+
+        public override string ToString()
+        {
+            return "Фрукт";
+        }
+
+        public override void ToConsole()
+        {
+            base.ToConsole();
+            Console.WriteLine("Свежесть: " + Freshness);
+            Console.WriteLine(new String('-', 25));
+        }
+    }
+
+    class Yogurt : Product
+    {
+        public int ExpirationDate { get; private set; }
+        public Yogurt(int expirationDate, string name, int price, string manufacturer)
+        {
+            ExpirationDate = expirationDate;
+            Name = name;
+            Price = price;
+            Manufacturer = manufacturer;
+        }
+
+        public override double GetDiscountPrice(User user)
+        {
+            return Price/2;
+        }
+
+        public override string ToString()
+        {
+            return "Йогурт";
+        }
+
+        public override void ToConsole()
+        {
+            base.ToConsole();
+            Console.WriteLine("Срок годности: " + ExpirationDate + " дней");
+            Console.WriteLine(new String('-', 25));
+        }
+    }
+
+}
+
+
+
---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿namespace Polimorfizm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User(
+                "Валера",
+                "Улица ..., дом ...",
+                10000,
+                300
+                );
+        
+            Console.WriteLine("Список товаров:");
+
+            Steak tomagafkSteak = new Steak(
+                "Томагафк", 
+                "Стейк-топор",
+                2200, 
+                "Мясник"
+                );
+
+            Steak shatoBrian = new Steak(
+                "Шато-бриан",
+                "Оу-май",
+                5000,
+                "Unknown"
+                );
+
+            Fruit banan = new Fruit(
+                0,
+                "Бананчик",
+                100, 
+                "Добрый банан"
+                );
+           
+            Yogurt yogurt = new Yogurt(
+                25, 
+                "Молочное чудо", 
+                85, 
+                "Unknown"
+                );
+
+            Product[] products = new Product[]
+            {
+                tomagafkSteak,
+                shatoBrian,
+                banan,
+                yogurt
+            };                                 //Произошел upcast
+
+            foreach (Product product in products)
+            {
+                product.ToConsole();
+            }
+
+            Informer informer = new Informer();
+
+            while(true)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Ну так {user.Name} у вас {user.Balance} монет");
+
+                for (int i = 0; i < products.Length; i++)
+                {
+                    Console.WriteLine($"Товар {i} {products[i].Name} по цене {products[i].Price}");
+                }
+                Console.WriteLine("Выберите номер товара и нажмите Enter:");
+
+                string str = Console.ReadLine();
+                int productNumber = Convert.ToInt32(str);
+
+                if (productNumber >= 0 && productNumber < products.Length)
+                {
+                    if (products[productNumber].Price < user.Balance)
+                    {
+                        informer.Buy(user, products[productNumber]);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Где деньги");
+                    }
+                } 
+                else
+                {
+                    Console.WriteLine("Такого нет, ты куда нажал");
+                }
+            }
+        }
+    }
+}
